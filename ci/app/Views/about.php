5eb628de62b5eb700f86efbc41bb685cb115e4fe
Fawaz Ibraheem <fawazming@gmail.com>
2022-10-08 16:30:11 +0100

    abt

--- ci/app/Views/about.php
+++ ci/app/Views/about.php
@@ -24,29 +24,125 @@
     <!-- Hero End -->
 
     <!-- Start -->
-    <section class="section">
+    <section class="section pb-0">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-6">
                     <div class="section-title">
-                        <h6 class="text-primary fw-normal">Welcome to Fronter</h6>
-                        <h4 class="title">Fronter Is The Perfect <br> Choice For Your Business <br> Software Needs.</h4>
+                        <h6 class="text-primary fw-normal">The Birth of PFA</h6>
+                        <h4 class="title">The idea of PFA was finalized and kick started on 16th September 2016.</h4>
                     </div>
                 </div><!--end col-->
 
                 <div class="col-md-6">
-                    <p class="text-muted mb-0">The most well-known dummy text is the originated in the 16th century. Lorem Ipsum is composed in a pseudo-Latin Latin. It contains a series of real Latin words. This ancient dummy text is also incomprehensible Latin script. The advantage of its Latin origin and the relative does not attract attention to itself or distract the viewer's attention from the layout.</p>
+                    <p class="text-muted mb-1">Pride of faith Academy school secondary school established in Sagamu and commenced operations officially on 16th September 2016. Pride of faith Academy is duly registered with the ministry of education science and technology Ogun state. The school is owned by Pride of Faith Foundation, a foundation whose focus is human capital development in the areas of Islamic education, health and community services.</p>
+                    <p class="text-muted mb-1">The motivation to venture into Islamic educational services within the ambit of Secondary education is premised on the fact that the whole of Remo Kingdom, with about 35 towns, did not have more than five Muslim secondary schools at that time. In addition, a preliminary mini survey conducted by a steering committee found that Muslim elites yearn for quality Islamic secondary education for their children and due to little of such an opportunity, they prefer to enroll their children in the conventional private secondary schools. </p>
+                    <p class="text-muted mb-0">Given this situation, the ball was set rolling by then chairman of the foundation in 2015, Mr. Lateef Olajide Ayilara. In the process, a lot of strategic meetings were held by the steering committee where basic components and requirements for the establishment of a secondary school were discussed reviewed debated and adopted. At the end of the process, the idea of PFA was finalized and kick started on 16th September 2016.</p>
                 </div><!--end col-->
             </div><!--end row-->
         </div><!--end container-->
-
-        <div class="container mt-100 mt-60">
+    </section>
+    <!-- Start -->
+    <section class="section pb-0" id="features">
+        <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12">
                     <div class="section-title text-center mb-4 pb-2">
                         <h6 class="text-primary fw-normal">Who we are?</h6>
-                        <h4 class="title mb-4">Business strategies and top <br> performance ideas</h4>
-                        <p class="text-muted para-desc mb-0 mx-auto">Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap html page.</p>
+                        <h4 class="title mb-4">Our Vision, Mission & Values</h4>
+                        <!-- <p class="text-muted para-desc mb-0 mx-auto">Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap html page.</p> -->
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="card features feature-primary feature-clean feature-transition p-4 border-0 shadow rounded overflow-hidden">
+                        <div class="icons text-center rounded">
+                            <i class="uil uil-presentation-line d-block rounded h4 mb-0"></i>
+                        </div>
+                        <div class="content mt-4">
+                            <a href="page-service-detail.html" class="h5 text-dark title">Our Vision</a>
+                            <p class="text-muted mt-3">We have a vision of students prepared and trained to make a different and visible impact on the community and global leadership system based on islamic faith, morality and sound academic training</p>
+                        </div>
+                        <div class="feature-ovelay-one"></div>
+                        <div class="feature-ovelay-two"></div>
+                        <div class="feature-ovelay-three"></div>
+                    </div>
+                </div>
+                <!--end col-->
+                <div class="col-lg-4 col-sm-6">
+                    <div class="card features feature-primary feature-clean feature-transition p-4 border-0 shadow rounded overflow-hidden">
+                        <div class="icons text-center rounded">
+                            <i class="uil uil-bill d-block rounded h4 mb-0"></i>
+                        </div>
+                        <div class="content mt-4">
+                            <a href="page-service-detail.html" class="h5 text-dark title">Our Mission</a>
+                            <p class="text-muted mt-3">To contribute to society through quality, knowledge-based education and islamic training by promoting a world class learning environment known for attracting and training young Muslims with the greatest potential to make a difference in all spheres of life.</p>
+                        </div>
+                        <div class="feature-ovelay-one"></div>
+                        <div class="feature-ovelay-two"></div>
+                        <div class="feature-ovelay-three"></div>
+                    </div>
+                </div>
+                <!--end col-->
+                <div class="col-lg-4 col-sm-6">
+                    <div class="card features feature-primary feature-clean feature-transition p-4 border-0 shadow rounded overflow-hidden">
+                        <div class="icons text-center rounded">
+                            <i class="uil uil-money-withdrawal d-block rounded h4 mb-0"></i>
+                        </div>
+                        <div class="content mt-4">
+                            <a href="page-service-detail.html" class="h5 text-dark title">Core Values</a>
+                            <ul class=" ">
+                                <li>Faith</li>
+                                <li>Integrity</li>
+                                <li>Intellectualism</li>
+                                <li>Self-Reliance</li>
+                                <li>Discipline</li>
+                            </ul>
+                        </div>
+                        <div class="feature-ovelay-one"></div>
+                        <div class="feature-ovelay-two"></div>
+                        <div class="feature-ovelay-three"></div>
+                    </div>
+                </div>
+                <!--end col-->
+                <div class="offset-md-2 col-md-8 col-sm-6">
+                    <div class="card features feature-primary feature-clean feature-transition p-4 border-0 shadow rounded overflow-hidden">
+                        <div class="icons text-center rounded">
+                            <i class="uil uil-money-withdrawal d-block rounded h4 mb-0"></i>
+                        </div>
+                        <div class="content mt-4">
+                            <a href="page-service-detail.html" class="h5 text-dark title">Our Core Services</a>
+                            <ul class=" ">
+                                <li>Quality Education and Sound Islamic Training</li>
+                                <li>Vocational and entrepreneurial skills</li>
+                                <li>Technical and knowledge-based education</li>
+                                <li>Leadership and glocalization skills</li>
+                                <li>Classical arabic training</li>
+                            </ul>
+                        </div>
+                        <div class="feature-ovelay-one"></div>
+                        <div class="feature-ovelay-two"></div>
+                        <div class="feature-ovelay-three"></div>
+                    </div>
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </section>
+    <!--end section-->
+    <section class="section">
+
+        <div class="container pt-0">
+            <div class="row justify-content-center py-0 my-0">
+                <div class="col-12">
+                    <div class="section-title text-center mb-4 pb-2">
+                        <!-- <h6 class="text-primary fw-normal">Who we are?</h6> -->
+                        <h4 class="title mb-4">Our Departments</h4>
                     </div>
                 </div><!--end col-->
             </div><!--end row-->
@@ -295,5 +391,6 @@
                 </div><!--end col-->
             </div><!--end row-->
         </div><!--end container-->
-    </section><!--end section-->
+    </section>
+    <!--end section-->
     <!-- End -->
